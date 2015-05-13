1ca88f16a7ca60713d8f100fdb9bb4abc63c092a
zaharenkoevgenii <zaharenko.evgenii@gmail.com>
2015-05-13 17:30:03 +0200

    file upload final1

--- MvcPL/Controllers/FileWorkController.cs
+++ MvcPL/Controllers/FileWorkController.cs
@@ -4,10 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Security.AccessControl;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Providers;
 using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
 using BLL.Interface.Entities;
 using BLL.Interface.Services;
 using MvcPL.Models;
@@ -29,6 +31,7 @@ namespace MvcPL.Controllers
         {
             var id = _uservice.GetAllUserEntities().First(user => user.UserName == User.Identity.Name).Id;
             return View(_fservice.GetAllFileEntities()
+                .OrderBy(f=>f.Created)
                 .Where(file=>file.OwnerId==id)
                 .Select(file => new FileViewModel()
                 {
@@ -52,6 +55,20 @@ namespace MvcPL.Controllers
                 if (upload != null)
                 {
                     string fileName = System.IO.Path.GetFileName(upload.FileName);
+                    int numb = 0;
+                    while ( _fservice.GetAllFileEntities()
+                        .Where(f1 => f1.OwnerId == _uservice.GetAllUserEntities().First(user => user.UserName == User.Identity.Name).Id)
+                        .Count(f2 => f2.Name == fileName)!= 0)
+                    {
+                        numb ++;
+                        string[] parts = fileName.Split('.');
+                        if (numb > 1) parts[0] = parts[0].Substring(0,parts[0].Length-2-numb.ToString().Length);
+                        fileName = parts[0] + "(" + numb + ").";
+                        for(int i=1;i<parts.Length;i++)
+                        {
+                            fileName += parts[i];
+                        }
+                    }
                     System.IO.Directory.CreateDirectory(Server.MapPath("~/Files/" + User.Identity.Name));
                     upload.SaveAs(Server.MapPath("~/Files/"+ User.Identity.Name+"/"+ fileName));
                     var bllFile = new FileEntity()
